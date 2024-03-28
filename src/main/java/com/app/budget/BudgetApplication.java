30e2681c3d5aaa18411ddc7b1741eefb463dff62
Jailson Anjos <jailsonanjos@hotmail.com>
2024-03-28 10:02:51 -0300

    feat(): Set up project

---
+++ src/main/java/com/app/budget/BudgetApplication.java
@@ -0,0 +1,13 @@
+package com.app.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/budget/BudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
