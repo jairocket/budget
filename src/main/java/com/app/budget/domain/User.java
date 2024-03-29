99752bc9ebf30a687949ec3dd7b484decb46c73d
Jailson Anjos <jailsonanjos@hotmail.com>
2024-03-29 12:47:53 -0300

    feat(user): Validate email and password

--- src/main/java/com/app/budget/domain/User.java
+++ src/main/java/com/app/budget/domain/User.java
@@ -2,6 +2,9 @@ package com.app.budget.domain;
 
 import com.app.budget.exceptions.UserCreationException;
 
+import java.util.Optional;
+import java.util.regex.Pattern;
+
 public class User {
     private final String email;
     private Long id;
@@ -9,14 +12,14 @@ public class User {
     private String password;
 
     public User(String name, String email, String password) {
-        validateIfUserConstructor(name, email, password);
+        validateUserConstructor(name, email, password);
         this.name = name;
         this.email = email;
         this.password = password;
     }
 
     public User(Long id, String name, String email, String password) {
-        validateIfUserConstructor(name, email, password);
+        validateUserConstructor(name, email, password);
         this.id = id;
         this.name = name;
         this.email = email;
@@ -47,20 +50,46 @@ public class User {
         return email;
     }
 
-    private void validateIfUserConstructor(String name, String email, String password) {
+    private boolean emailIsValid(String email) {
+        email = Optional.ofNullable(email).orElseThrow(() -> new UserCreationException("User email cannot be null"));
+
+        return Pattern.compile("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+                .matcher(email)
+                .matches();
+    }
+
+    private void validateName(String name) {
         if (name == null) {
             throw new UserCreationException("User name cannot be null");
         }
-        if (email == null) {
-            throw new UserCreationException("User email cannot be null");
+
+        if (name.length() < 3) {
+            throw new UserCreationException("User name should have at least three characters");
         }
-        if (password == null) {
-            throw new UserCreationException("User password cannot be null");
+    }
+
+    private boolean passwordIsValid(String password) {
+        password = Optional.ofNullable(password).orElseThrow(() -> new UserCreationException("User password cannot be null"));
+
+        return Pattern.compile(
+                        "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,20}$",
+                        Pattern.CASE_INSENSITIVE
+                )
+                .matcher(password)
+                .matches();
+    }
+
+    private void validateUserConstructor(String name, String email, String password) {
+        validateName(name);
+
+        if (!emailIsValid(email)) {
+            throw new UserCreationException("Inform a valid email");
         }
 
-//        if(name.length() < 5) {
-//            throw new UserCreationException("User name cannot be null");
-//        }
+        if (!passwordIsValid(password)) {
+            throw new UserCreationException("Inform a valid password");
+        }
+
     }
 
 
--- src/test/java/com/app/budget/domain/UserTest.java
+++ src/test/java/com/app/budget/domain/UserTest.java
@@ -11,7 +11,7 @@ public class UserTest {
         Long id = 1L;
         String name = "New User";
         String email = "new.user@budget.com";
-        String password = "ThePassWord";
+        String password = "P@inK1ller";
 
         User user = new User(id, name, email, password);
 
@@ -21,7 +21,18 @@ public class UserTest {
     }
 
     @Test
-    public void shouldThrowErrorIfNameIsNull() {
+    public void shouldThrowExceptionIfPassWordIsInvalid() {
+        Long id = 1L;
+        String name = "New User";
+        String email = "new.user@budget.com";
+        String password = "ThePassWord";
+
+        UserCreationException exception = assertThrows(UserCreationException.class, () -> new User(id, name, email, password));
+        assertEquals("Inform a valid password", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionIfNameIsNull() {
         String email = "new.user@budget.com";
         String password = "ThePassWord";
 
@@ -30,7 +41,7 @@ public class UserTest {
     }
 
     @Test
-    public void shouldThrowErrorIfEmailIsNull() {
+    public void shouldThrowExceptionIfEmailIsNull() {
         String name = "New User";
         String password = "ThePassWord";
 
@@ -39,11 +50,31 @@ public class UserTest {
     }
 
     @Test
-    public void shouldThrowErrorIfPassWordIsNull() {
+    public void shouldThrowExceptionIfEmailIsInvalid() {
+        String name = "New User";
+        String email = "new.user#budget.com";
+        String password = "ThePassWord";
+
+        UserCreationException exception = assertThrows(UserCreationException.class, () -> new User(name, email, password));
+        assertEquals("Inform a valid email", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionIfPassWordIsNull() {
         String name = "New User";
         String email = "new.user@budget.com";
 
         UserCreationException exception = assertThrows(UserCreationException.class, () -> new User(name, email, null));
         assertEquals("User password cannot be null", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionIfNameHasLessThanThreeCharacters() {
+        String name = "Ne";
+        String email = "new.user@budget.com";
+        String password = "ThePassWord";
+
+        UserCreationException exception = assertThrows(UserCreationException.class, () -> new User(name, email, password));
+        assertEquals("User name should have at least three characters", exception.getMessage());
     }
 }
