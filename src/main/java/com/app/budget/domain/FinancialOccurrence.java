4929630a6199db157cf07356def9b8845ebf6ecc
Jailson Anjos <jailsonanjos@hotmail.com>
2024-03-30 14:42:52 -0300

    feat(domain): Add Expense

---
+++ src/main/java/com/app/budget/domain/FinancialOccurrence.java
@@ -0,0 +1,113 @@
+package com.app.budget.domain;
+
+import com.app.budget.exceptions.FinancialOccurrenceException;
+import org.apache.commons.math3.util.Precision;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.Set;
+
+abstract class FinancialOccurrence {
+    private Long id;
+    private Set<Category> categories;
+    private String title;
+    private Double value;
+    private LocalDate dueDate;
+
+    public FinancialOccurrence(Long id, Set<Category> categories, String title, Double value, LocalDate dueDate) {
+        validateCategories(categories);
+        validateTitle(title);
+        validateValue(value);
+
+        this.id = id;
+        this.categories = categories;
+        this.title = title;
+        this.value = round(value);
+        this.dueDate = dueDate;
+    }
+
+    public FinancialOccurrence(Set<Category> categories, String title, Double value, LocalDate dueDate) {
+        validateCategories(categories);
+        validateTitle(title);
+        validateValue(value);
+
+        this.categories = categories;
+        this.title = title;
+        this.value = round(value);
+        this.dueDate = dueDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+
+    private void validateCategories(Set<Category> categories) {
+        categories = Optional.ofNullable(categories).orElseThrow(() -> new FinancialOccurrenceException("Categories cannot be null"));
+
+        if (categories.isEmpty()) {
+            Category others = new Category("Others");
+            categories.add(others);
+        }
+    }
+
+    private void validateTitle(String title) {
+        if (title == null) {
+            throw new FinancialOccurrenceException("Title cannot be null");
+        }
+
+        if (title.length() < 3) {
+            throw new FinancialOccurrenceException("Title should have at least three characters");
+        }
+    }
+
+    private void validateValue(Double value) {
+        value = Optional.ofNullable(value).orElseThrow(() -> new FinancialOccurrenceException("Value cannot be null"));
+
+        if (value < 0.00) {
+            throw new FinancialOccurrenceException("Value should not be lower than zero");
+        }
+    }
+
+    private Double round(Double value) {
+        return Precision.round(value, 2);
+    }
+
+    private void validateDueDate(LocalDate dueDate) {
+        if (dueDate == null) {
+            throw new FinancialOccurrenceException("Due date should not be lower than zero");
+        }
+    }
+
+}
---
+++ src/test/java/com/app/budget/domain/ExpenseTest.java
@@ -0,0 +1,152 @@
+package com.app.budget.domain;
+
+import com.app.budget.enums.ExpenseStatus;
+import com.app.budget.exceptions.FinancialOccurrenceException;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class ExpenseTest {
+    @Test
+    public void shouldBeAbleToCreateExpense() {
+        Long id = 1L;
+        Set<Category> categories = new HashSet<>();
+        categories.add(new Category("Transportation"));
+        String title = "Uber";
+        Double value = 35.90;
+        LocalDate dueDate = LocalDate.of(2024, 2, 1);
+        ExpenseStatus status = ExpenseStatus.PENDING;
+
+        Expense expense = new Expense(id, categories, title, value, dueDate, status);
+
+        assertTrue(expense.getId() == 1);
+        assertEquals(1, expense.getCategories().size());
+        assertEquals("Transportation", expense.getCategories().stream().toList().get(0).getName());
+        assertEquals("Uber", expense.getTitle());
+        assertTrue(expense.getValue() == 35.90);
+        assertEquals("2024-02-01", expense.getDueDate().toString());
+        assertEquals(ExpenseStatus.PENDING, expense.getStatus());
+    }
+
+    @Test
+    public void shouldValidateCategories() {
+        Long id = 1L;
+        Set<Category> categories = new HashSet<>();
+        String title = "Uber";
+        Double value = 35.90;
+        LocalDate dueDate = LocalDate.of(2024, 2, 1);
+        ExpenseStatus status = ExpenseStatus.PENDING;
+
+        Expense expense = new Expense(id, categories, title, value, dueDate, status);
+
+        assertTrue(expense.getId() == 1);
+        assertEquals("Others", expense.getCategories().stream().toList().get(0).getName());
+        assertEquals("Uber", expense.getTitle());
+        assertTrue(expense.getValue() == 35.90);
+        assertEquals("2024-02-01", expense.getDueDate().toString());
+        assertEquals(ExpenseStatus.PENDING, expense.getStatus());
+    }
+
+    @Test
+    public void shouldThrowExceptionIfCategoriesIsNull() {
+        Double value = 35.90;
+        String title = "Uber";
+        LocalDate dueDate = LocalDate.of(2024, 2, 1);
+        ExpenseStatus status = ExpenseStatus.PENDING;
+
+        FinancialOccurrenceException exception = assertThrows(FinancialOccurrenceException.class, () -> new Expense(null, title, value, dueDate, status));
+        assertEquals("Categories cannot be null", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionIfTitleIsNull() {
+        Double value = 35.90;
+        LocalDate dueDate = LocalDate.of(2024, 2, 1);
+        ExpenseStatus status = ExpenseStatus.PENDING;
+        Set<Category> categories = new HashSet<>();
+
+        FinancialOccurrenceException exception = assertThrows(FinancialOccurrenceException.class, () -> new Expense(categories, null, value, dueDate, status));
+        assertEquals("Title cannot be null", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionIfTitleHasLessThanThreeCharacters() {
+        String title = "Ub";
+        Double value = 35.90;
+        LocalDate dueDate = LocalDate.of(2024, 2, 1);
+        ExpenseStatus status = ExpenseStatus.PENDING;
+        Set<Category> categories = new HashSet<>();
+
+        FinancialOccurrenceException exception = assertThrows(FinancialOccurrenceException.class, () -> new Expense(categories, title, value, dueDate, status));
+        assertEquals("Title should have at least three characters", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionIfValueIsNull() {
+        String title = "Uber";
+        LocalDate dueDate = LocalDate.of(2024, 2, 1);
+        ExpenseStatus status = ExpenseStatus.PENDING;
+        Set<Category> categories = new HashSet<>();
+
+        FinancialOccurrenceException exception = assertThrows(FinancialOccurrenceException.class, () -> new Expense(categories, title, null, dueDate, status));
+        assertEquals("Value cannot be null", exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionIfValueLowerThanZero() {
+        String title = "Uber";
+        LocalDate dueDate = LocalDate.of(2024, 2, 1);
+        ExpenseStatus status = ExpenseStatus.PENDING;
+        Set<Category> categories = new HashSet<>();
+        Double value = -40.99;
+
+        FinancialOccurrenceException exception = assertThrows(FinancialOccurrenceException.class, () -> new Expense(categories, title, value, dueDate, status));
+        assertEquals("Value should not be lower than zero", exception.getMessage());
+    }
+
+    @Test
+    public void shouldFormatValueToTwoDecimalDigits() {
+        Long id = 1L;
+        Set<Category> categories = new HashSet<>();
+        categories.add(new Category("Transportation"));
+        String title = "Uber";
+        Double value = 35.9563;
+        LocalDate dueDate = LocalDate.of(2024, 2, 1);
+        ExpenseStatus status = ExpenseStatus.PENDING;
+
+        Expense expense = new Expense(id, categories, title, value, dueDate, status);
+
+        assertTrue(expense.getValue() == 35.96);
+
+    }
+
+    @Test
+    public void shouldSetStatusToPendingIfStatusIsNull() {
+        Long id = 1L;
+        Set<Category> categories = new HashSet<>();
+        categories.add(new Category("Transportation"));
+        String title = "Uber";
+        Double value = 35.9563;
+        LocalDate dueDate = LocalDate.of(2024, 2, 1);
+
+        Expense expense = new Expense(id, categories, title, value, dueDate, null);
+
+        assertTrue(expense.getStatus() == ExpenseStatus.PENDING);
+    }
+
+    @Test
+    public void shouldThrowExceptionIfDueDateIsNull() {
+        String title = "Uber";
+        ExpenseStatus status = ExpenseStatus.PENDING;
+        Set<Category> categories = new HashSet<>();
+        Double value = -40.99;
+
+        FinancialOccurrenceException exception = assertThrows(FinancialOccurrenceException.class, () -> new Expense(categories, title, value, null, status));
+        assertEquals("Value should not be lower than zero", exception.getMessage());
+    }
+
+}
