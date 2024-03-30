3a0029af24a2d4875c4bf62a22bb85764587c375
Jailson Anjos <jailsonanjos@hotmail.com>
2024-03-30 11:43:36 -0300

    feat(category): Create Categories

---
+++ src/main/java/com/app/budget/domain/Category.java
@@ -0,0 +1,43 @@
+package com.app.budget.domain;
+
+import com.app.budget.exceptions.CategoryException;
+
+import java.util.Optional;
+
+public class Category {
+    private Long id;
+    private String name;
+
+    public Category(Long id, String name) {
+        validateName(name);
+
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category(String name) {
+        validateName(name);
+
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        name = Optional.ofNullable(name).orElseThrow(() -> new CategoryException("Category name cannot be null"));
+
+        if (name.length() < 3) {
+            throw new CategoryException("User category should have at least three characters");
+        }
+    }
+}
