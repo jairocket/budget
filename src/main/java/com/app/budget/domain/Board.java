00e0a0de127e5a24247c35b61c61f47cce3ad4e0
Jailson Anjos <jailsonanjos@hotmail.com>
2024-04-01 23:09:45 -0300

    feat(domain): Create board

---
+++ src/main/java/com/app/budget/domain/Board.java
@@ -0,0 +1,55 @@
+package com.app.budget.domain;
+
+import com.app.budget.exceptions.BoardException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Board {
+    private final User user;
+    private Long id;
+    private List<Income> incomes = new ArrayList<>();
+    private List<Expense> expenses = new ArrayList<>();
+
+    public Board(Long id, User user) {
+        user = Optional.ofNullable(user).orElseThrow(() -> new BoardException("User cannot be null"));
+
+        this.id = id;
+        this.user = user;
+    }
+
+    public Board(User user) {
+        user = Optional.ofNullable(user).orElseThrow(() -> new BoardException("User cannot be null"));
+
+        this.user = user;
+    }
+
+    public List<Income> getIncomes() {
+        return incomes;
+    }
+
+    public void setIncomes(List<Income> incomes) {
+        incomes = Optional.ofNullable(incomes).orElseThrow(() -> new BoardException("Incomes cannot be null"));
+
+        this.incomes = incomes;
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(List<Expense> expenses) {
+        expenses = Optional.ofNullable(expenses).orElseThrow(() -> new BoardException("Expenses cannot be null"));
+
+        this.expenses = expenses;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
---
+++ src/main/java/com/app/budget/exceptions/BoardException.java
@@ -0,0 +1,7 @@
+package com.app.budget.exceptions;
+
+public class BoardException extends BusinessException {
+    public BoardException(String errorMessage) {
+        super(errorMessage);
+    }
+}
