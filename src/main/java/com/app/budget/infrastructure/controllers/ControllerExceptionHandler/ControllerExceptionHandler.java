2c7021a19d6b7a432340d9f2eafea3fbf8c4c13a
Jailson Anjos <jailsonanjos@hotmail.com>
2024-04-20 00:28:24 -0300

    feat(controller): Add controller exception handler

---
+++ src/main/java/com/app/budget/infrastructure/controllers/ControllerExceptionHandler/ControllerExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.app.budget.infrastructure.controllers.ControllerExceptionHandler;
+
+import com.app.budget.core.exceptions.UserException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<StandardError> userError(UserException exception, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError error = new StandardError(Instant.now(), status.value(), exception.getMessage(), request.getContextPath());
+        return ResponseEntity.status(status).body(error);
+    }
+
+
+}
