45e360d88c39410f9217b9cc73b99c57169a9c60
Jailson Anjos <jailsonanjos@hotmail.com>
2024-08-10 19:34:29 -0300

    chore(): Fix lint issues

--- src/main/java/com/app/budget/infrastructure/gateways/UserDTOMapper.java
+++ src/main/java/com/app/budget/infrastructure/gateways/UserDTOMapper.java
@@ -1,8 +1,5 @@
 package com.app.budget.infrastructure.gateways;
 
-import com.app.budget.core.domain.User;
-import com.app.budget.core.enums.UserRole;
-import com.app.budget.infrastructure.controllers.dto.UserRegisterDTO;
 import com.app.budget.infrastructure.controllers.dto.UserRegisterResponseDTO;
 import com.app.budget.infrastructure.persistence.entities.UserEntity;
 import org.springframework.stereotype.Component;
@@ -10,10 +7,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserDTOMapper {
-    public User toDomain(UserRegisterDTO dto, UserRole role) {
-        return new User(dto.name(), dto.email(), dto.password(), role);
-    }
-
     public UserRegisterResponseDTO toResponse(UserEntity user) {
         return new UserRegisterResponseDTO(user.getId(), user.getName(), user.getEmail(), user.getRole());
     }
